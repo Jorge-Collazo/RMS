3cb32b2cc59b2e87f3bb24b65b8030f5daa32722
Nicholas Garcia <ngarcia158@west-mec.org>
2021-02-23 15:19:00 -0700

    First halfof populating db

--- faker.js
+++ faker.js
@@ -3,3 +3,58 @@ var faker = require('faker');
 var randomName = faker.name.findName(); // Rowan Nikolaus
 var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
 var randomCard = faker.helpers.createCard(); // random contact card containing many properties
+
+app.post('/',(req,res)=>{  
+    for(var i=0; i<10;i++){  
+      var fakke = new fakerModel({  
+      firstname:faker.name.firstName(),  
+      lastname:faker.name.lastName(),  
+      phonenumber:faker.phone.phoneNumber(),  
+      city:faker.address.city(),  
+      state:faker.address.state(),  
+      country:faker.address.country()  
+        })  
+        fakke.save((err,data)=>{  
+             if(err){
+            console.log('error occured',err)  
+            }
+        })  
+    }  
+    res.redirect('/');  
+  })  
+  app.post('/',(req,res)=>{  
+    for(var i=0; i<10;i++){  
+      var fakke = new fakerModel({  
+      firstname:faker.name.firstName(),  
+      lastname:faker.name.lastName(),  
+      phonenumber:faker.phone.phoneNumber(),  
+      city:faker.address.city(),  
+      state:faker.address.state(),  
+      country:faker.address.country()  
+        })  
+        fakke.save((err,data)=>{  
+             if(err){
+            console.log('error occured',err)  
+            }
+        })  
+    }  
+    res.redirect('/');  
+  })  
+  app.post('/',(req,res)=>{  
+    for(var i=0; i<10;i++){  
+      var fakke = new fakerModel({  
+      firstname:faker.name.firstName(),  
+      lastname:faker.name.lastName(),  
+      phonenumber:faker.phone.phoneNumber(),  
+      city:faker.address.city(),  
+      state:faker.address.state(),  
+      country:faker.address.country()  
+        })  
+        fakke.save((err,data)=>{  
+             if(err){
+            console.log('error occured',err)  
+            }
+        })  
+    }  
+    res.redirect('/');  
+  })  
