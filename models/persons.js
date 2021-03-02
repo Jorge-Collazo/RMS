d2117f879a7a3902368380a4d7fb911fb121da4d
Jorge <jcollazo151@west-mec.org>
2021-03-02 15:24:53 -0700

    added more schemas

--- models/persons.js
+++ models/persons.js
@@ -8,7 +8,9 @@ var AuthorSchema = new Schema(
     {
         first_name: { type: String, required: true, maxlength: 100 },
         middle_initial:{type: String, required: false, maxlength: 2},
-        family_name: { type: String, required: true, maxlength: 100 },
+        last_name: { type: String, required: true, maxlength: 100 },
+        social_security_number:{},
+        age:{},
         date_of_birth: { type: Date },
         date_of_death: { type: Date },
     }
