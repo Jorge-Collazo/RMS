00809aea324bc7cd3d7c661b549602908a4b7cca
Jorge <jcollazo151@west-mec.org>
2021-03-02 15:24:13 -0700

    Trying to learn how Schema works

---
+++ models/persons.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+const { DateTime } = require("luxon");  //for date handling
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+    {
+        first_name: { type: String, required: true, maxlength: 100 },
+        middle_initial:{type: String, required: false, maxlength: 2},
+        family_name: { type: String, required: true, maxlength: 100 },
+        date_of_birth: { type: Date },
+        date_of_death: { type: Date },
+    }
+);
