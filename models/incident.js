effff2ba94d9e65dddd4420bf9fddcc21f624450
Jorge <jcollazo151@west-mec.org>
2021-03-04 14:25:48 -0700

    added enum's to People model

--- models/incident.js
+++ models/incident.js
@@ -6,28 +6,50 @@ var Schema = mongoose.Schema;
 
 var IncidentSchema = new Schema(
     {
-        occurence_time: {},
-        occurence_date: {},
-        name: {},
-        social_security_number: {},
-        date _of_birth: {},
-        license_plate_state: {},
-        officer_name: {},
-        Report_Date_&_Time: {},
-        serial_number: {},
-        Code: {},
-        Incident_Type: {},
-        Location: {},
-        License_Number: {},
-        License_state: {},
-        License_date: {},
-        License_Plate_#: {},
-        Officer_serial_Number: {},
-        Value_of_stolen/damage: {},
-        Date: {},
-        Gang_affiliation: {},
-        Hazard: {},
-        Hazard_information: {},
-        Narriative: {},
+        occurence_time: {type: String, required: true, maxlength:7, minlength:7},
+
+        occurence_date: {type: Date, required: true,},
+
+        name: {type: String, required: true, maxlength:100},
+
+        social_security_number: {type: Number, required: true, maxlength:9, minlength:9},
+
+        date_of_birth: {type: Date, required: true,},
+
+        license_plate_state: {type: String, required: true, maxlength:2, enum: ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD','TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY',], default: 'AL'},
+
+        officer_name: {type: String, required: true, maxlength:100},
+
+        report_date: {type: Date, required: true,},
+
+        report_time: {type: String, required: true,  maxlength:7, minlength:7},
+
+        code: {type: Boolean, required: true,},
+
+        incident_type: {type: String, required: true,},
+
+        location: {type: String, required: true,},
+
+        license_number: {type: Number, required: true,},
+
+        license_state: {type: String, required: true,},
+
+        license_date: {type: Date, required: true,},
+
+        license_plate_number: {type: Number, required: true,},
+
+        officer_serial_number: {type: Number, required: true,},
+
+        value: {type: Number, required: false,},
+
+        serial_number: {type: Number, required: true,},
+
+        gang_affiliation: {type: Boolean, required: false,},
+
+        hazard: {type: Boolean, required: false,},
+
+        hazard_information: {type: String, required: false,},
+
+        narriative: {type: String, required: true,},
     }
 );
